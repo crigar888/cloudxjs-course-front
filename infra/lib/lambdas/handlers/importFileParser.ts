93657b5adcbf15d0782ddfc833f71494b65075d0
Cristian Garcia <crigar888@gmail.com>
2025-10-15 13:48:46 -0500

    fix: console logs removed

--- infra/lib/lambdas/handlers/importFileParser.ts
+++ infra/lib/lambdas/handlers/importFileParser.ts
@@ -7,13 +7,10 @@ const sqs = new SQS();
 const SQS_URL = process.env.SQS_URL!;
 
 export const main: Handler = async (event: S3Event) => {
-  console.log("Received S3 event:", JSON.stringify(event, null, 2));
 
   for (const record of event.Records) {
     const bucket = record.s3.bucket.name;
     const key = decodeURIComponent(record.s3.object.key.replace(/\+/g, " "));
-
-    console.log(`Processing file from S3: ${bucket}/${key}`);
 
     try {
       const s3Stream = s3
@@ -26,30 +23,24 @@ export const main: Handler = async (event: S3Event) => {
         s3Stream
           .pipe(csv())
           .on("data", async (data) => {
-            console.log("Parsed record:", data);
 
             const params = {
               QueueUrl: SQS_URL,
               MessageBody: JSON.stringify(data),
             };
 
-            const sendPromise = sqs.sendMessage(params).promise()
-              .then((res) => console.log("✅ Sent to SQS:", res.MessageId))
-              .catch((err) => console.error("❌ Failed to send message:", err));
+            const sendPromise = sqs.sendMessage(params).promise();
             sendMessagePromises.push(sendPromise);
           })
           .on("end", async  () => {
-            console.log("CSV parsing completed.");
             await Promise.all(sendMessagePromises);
             resolve();
           })
           .on("error", (err) => {
-            console.error("Error while parsing CSV:", err);
             reject(err);
           });
       });
     } catch (err) {
-      console.error("Error processing file:", err);
       throw err;
     }
   }
